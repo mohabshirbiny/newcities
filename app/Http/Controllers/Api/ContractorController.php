72a42227bef3be1197dd23eb93cd6c8f3916fd9d
Mohab Elshirbiny <mohabelshirbiny@gmail.com>
2020-10-13 02:02:43 +0300

    'contractors'

--- app/Http/Controllers/Api/ContractorController.php
+++ app/Http/Controllers/Api/ContractorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Contractor;
+use App\ContractorCategory;
 use App\Http\Controllers\Controller;
 use App\SectionData;
 
@@ -16,11 +17,14 @@ class ContractorController extends Controller
         $contractors = Contractor::query()->with("category")
                         ->Where('contractor_category_id','LIKE',$contractor_category_id)
                         ->get();
+                        
+        $contractors = ContractorCategory::all();
 
         $section = SectionData::where('model','Contractor')->first();
         
         $data = [
             "contractors" => $contractors,
+            "contractor_categories" => $contractors,
             "gallery" => ($section)?$section->section_gallery : (object)[],
         ];
 
